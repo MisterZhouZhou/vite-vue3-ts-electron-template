09a13d31f169c35e516950c3384b9d1cfb2acfca
MisterZhou <16619930394@163.com>
2022-04-07 17:56:41 +0800

    Update service.ts

--- src/api/service.ts
+++ src/api/service.ts
@@ -22,32 +22,9 @@ export const fetchRandMusic = () => {
   return request.get(URLS.randomMusic)
 }
 
-//v1/h5Bundle/history/${env}/${bundleName}/1/100
-// 获取历史离线包列表
+// 获取列表
 export const getHistoryBundleList = async (bundleName: string, env: string) => {
   const res: ResponseIns = await request.get(`${urlHost}/${env}/${bundleName}/1/100`)
-  const { code, data, msg } = res
-  if (code !== 10000) {
-    console.warn('$-请求失败：', msg)
-    return null
-  }
-  return data
-}
-
-// 获取离线包列表
-export const postBundleList = async () => {
-  const res: ResponseIns = await request.post(
-    '/srn-hub/v1/client/h5Bundle',
-    {
-      dependencies: {
-        h5_souche_24330: {
-          env: 'debug',
-          version: '1.9.4-beta.1647225252826'
-        }
-      }
-    },
-    { headers: { appname: 'cheyipai_business' } }
-  )
   const { code, data, msg } = res
   if (code !== 10000) {
     console.warn('$-请求失败：', msg)
